189325db3c0e98d5de87626b041b94276a3c87c1
David <d.weckerly@gmail.com>
2022-07-13 18:45:46 -0500

    initial stubbing for character Attributes

---
+++ Assets/Scripts/Core/Attributes.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Attribute
+{
+    Brains,
+    Brawn,
+    Guile,
+    Guts
+}
+
+public class Attributes : MonoBehaviour
+{
+    [SerializeField] int baseBrains;
+    [SerializeField] int baseBrawn;
+    [SerializeField] int baseGuile;
+    [SerializeField] int baseGuts;
+
+    private int currentBrains;
+    private int currentBrawn;
+    private int currentGuile;
+    private int currentGuts;
+
+    private Dictionary<Attribute, int> attrLookup;
+
+    private int maxHP;
+    private int currentHP;
+
+    private void Awake() 
+    {
+        currentBrains = baseBrains;
+        currentBrawn = baseBrawn;
+        currentGuile = baseGuile;
+        currentGuts = baseGuts;
+        maxHP = CalculateMaxHP();
+        currentHP = maxHP;
+
+        attrLookup = new Dictionary<Attribute, int> ()
+        {
+            { Attribute.Brains, currentBrains },
+            { Attribute.Brawn, currentBrawn },
+            { Attribute.Guile, currentGuile },
+            { Attribute.Guts, currentGuts },
+        };
+    }
+
+    private int CalculateMaxHP()
+    {
+        return 10 + baseGuts;
+    }
+
+    public int GetStat (Attribute attribute)
+    {
+        return attrLookup[attribute];
+    }
+
+    public int GetHP()
+    {
+        return currentHP;
+    }
+
+    public void ChangeStat (Attribute attribute, int amount)
+    {
+        attrLookup[attribute] += amount;
+    }
+
+    public void ChangeHP(int amount)
+    {
+        currentHP += amount;
+        if (currentHP < 0)
+        {
+            currentHP = 0;
+            Die();
+        }
+        if (currentHP > maxHP) currentHP = maxHP;
+    }
+
+    public void Die()
+    {
+        Debug.Log("this guy ded.");
+    }
+}
