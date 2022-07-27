bebf82b09b3226891f24ad8d6aa2db884e7b0fa7
David <d.weckerly@gmail.com>
2022-07-27 16:57:49 -0500

    GOON GEN scene

---
+++ Assets/Scripts/GoonGeneration/GoonGenerator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoonGenerator : MonoBehaviour
+{
+    public GoonParts goonParts;
+    public Transform headBone;
+
+    private void Start() 
+    {
+        SetColors(goonParts.SkinMaterial, goonParts.SkinColors, goonParts.SkinShadeColors);
+        SetColors(goonParts.NoseMaterial, goonParts.NoseColors, goonParts.NoseShadeColors);
+        SetColors(goonParts.HairMaterial, goonParts.HairColors, goonParts.HairShadeColors);
+        SetTexutre(goonParts.EyeMaterial, goonParts.Eyes);
+        SetTexutre(goonParts.MouthMaterial, goonParts.Mouths);
+        AddParts(goonParts.Ears);
+        AddParts(goonParts.Hair);
+        AddParts(goonParts.Noses);
+    }
+
+    private void SetColors(Material mat, Color[] baseColors, Color[] shadeColors)
+    {
+        int index = Random.Range(0, baseColors.Length);
+        mat.color = baseColors[index];
+        mat.SetColor("_ColorDim", shadeColors[index]);
+    }
+
+    private void SetTexutre(Material mat, Texture[] textures)
+    {
+        int index = Random.Range(0, textures.Length);
+        mat.mainTexture = textures[index];
+    }
+
+    private void AddParts(GameObject[] gos)
+    {
+        int index = Random.Range(0, gos.Length);
+        Instantiate(gos[index], headBone);
+    }
+}
