4a9c392cff26f98027e6145cb4bb03e91cca70d1
David <d.weckerly@gmail.com>
2022-07-27 10:55:28 -0500

    created Armor SO

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Weapon", menuName = "Items/Weapons/New Weapon", order = 1)]
+[CreateAssetMenu(fileName = "Weapon", menuName = "Items/New Weapon", order = 2)]
 public class Weapon : Item
 {
     public GameObject weaponPrefab = null;
---
+++ Assets/Scripts/Core/Armor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BodyMapping
+{
+    Head,
+    LeftShoulder,
+    RightShoulder
+}
+
+[System.Serializable]
+public class ArmorBodyMap
+{
+    public GameObject armorPrefab;
+    public BodyMapping bodyPositionReference;
+}
+
+[CreateAssetMenu(fileName = "Armor", menuName = "Items/New Armor", order = 1)]
+public class Armor : Item
+{
+    public ArmorBodyMap[] ArmorObjects;
+}
