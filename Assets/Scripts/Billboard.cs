50b926d9423d16a0d239617f7ac1ed584c427f81
David <d.weckerly@gmail.com>
2022-08-22 12:05:58 -0500

    billboard and interact prompt prefab

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    Camera mainCam;
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(mainCam.transform);
+    }
+}
