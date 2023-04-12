7080274e450543c3635ece5385cb953f72f4d778
David <d.weckerly@gmail.com>
2023-04-12 11:07:16 -0500

    fade scene and music at start

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
     public string sceneName;
 
     public InputReader inputReader;
+    public AudioSource music;
+
+    public Image fadeImage;
+
+    float fadeTime = 3f;
+    float fadeRate = 0.1f;
 
     private void Start() 
     {
@@ -21,6 +28,21 @@ public class SceneChanger : MonoBehaviour
 
     public void ChangeScene()
     {
+        StartCoroutine(FadeMusicAndScene());
+    }
+
+    public IEnumerator FadeMusicAndScene()
+    {
+        while (music.isPlaying)
+        {
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fadeImage.color.a + (Time.deltaTime / fadeTime));
+            music.volume -= Time.deltaTime / fadeTime;
+            if (music.volume <= 0)
+            {
+                music.Stop();
+            }
+            yield return null;
+        }
         SceneManager.LoadScene(sceneName);
     }
 }
