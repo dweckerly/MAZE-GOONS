93b38d663dde7b131bedad3a3988a648e5ef07ef
dweckerly <d.weckerly@gmail.com>
2023-04-22 20:13:00 -0500

    changed to color32

--- Assets/Scripts/ColorButtons.cs
+++ Assets/Scripts/ColorButtons.cs
@@ -5,27 +5,47 @@ using UnityEngine;
 public class ColorButtons : MonoBehaviour
 {
     public Material SkinMaterial;
-    public Material EarMaterial;
     public Material HairMaterial;
     public Material NoseMaterial;
 
-    void ChangeSkinColor(Color primary, Color secondary)
+    public void SkinButton0()
     {
-        SkinMaterial.color = primary;
-        EarMaterial.color = primary;
-        SkinMaterial.SetColor("_ColorDim", secondary);
-        EarMaterial.SetColor("_ColorDim", secondary);
+        Color32 primary = new Color32(188, 153, 105, 255);
+        Color32 secondary = new Color32(113, 93, 67, 255);
+        ChangeMaterialColor(SkinMaterial, primary, secondary);
     }
 
-    void ChangeHairColor(Color primary, Color secondary)
+    public void SkinButton1()
     {
-        HairMaterial.color = primary;
-        HairMaterial.SetColor("_ColorDim", secondary);
+        Color32 primary = new Color32(188, 119, 105, 255);
+        Color32 secondary = new Color32(113, 62, 52, 255);
+        ChangeMaterialColor(SkinMaterial, primary, secondary);
     }
 
-    void ChangeNoseColor(Color primary, Color secondary)
+    public void SkinButton2()
     {
-        NoseMaterial.color = primary;
-        NoseMaterial.SetColor("_ColorDim", secondary);
+        Color32 primary = new Color32(245, 220, 184, 255);
+        Color32 secondary = new Color32(96, 87, 75, 255);
+        ChangeMaterialColor(SkinMaterial, primary, secondary);
+    }
+
+    public void SkinButton3()
+    {
+        Color32 primary = new Color32(173, 111, 78, 255);
+        Color32 secondary = new Color32(79, 51, 36, 255);
+        ChangeMaterialColor(SkinMaterial, primary, secondary);
+    }
+
+    public void SkinButton4()
+    {
+        Color32 primary = new Color32(173, 111, 78, 255);
+        Color32 secondary = new Color32(79, 51, 36, 255);
+        ChangeMaterialColor(SkinMaterial, primary, secondary);
+    }
+
+    void ChangeMaterialColor(Material material, Color primary, Color secondary)
+    {
+        material.color = primary;
+        material.SetColor("_ColorDim", secondary);
     }
 }
