44516cadbb0f02c0f63b7bb53b1b5aa914e3feb3
dweckerly <d.weckerly@gmail.com>
2023-04-23 09:42:49 -0500

    refactored color change buttons

--- Assets/Scripts/ColorButtons.cs
+++ Assets/Scripts/ColorButtons.cs
@@ -8,65 +8,18 @@ public class ColorButtons : MonoBehaviour
     public Material HairMaterial;
     public Material NoseMaterial;
 
-    public void SkinButton0()
+    public ShadedColor[] SkinColors;
+    public ShadedColor[] HairColors;
+    public ShadedColor[] NoseColors;
+
+    public void SkinButton(int index)
     {
-        Color32 primary = new Color32(188, 153, 105, 255);
-        Color32 secondary = new Color32(113, 93, 67, 255);
-        ChangeMaterialColor(SkinMaterial, primary, secondary);
+        ChangeMaterialColor(SkinMaterial, index);
     }
 
-    public void SkinButton1()
+    void ChangeMaterialColor(Material material, int index)
     {
-        Color32 primary = new Color32(188, 119, 105, 255);
-        Color32 secondary = new Color32(113, 62, 52, 255);
-        ChangeMaterialColor(SkinMaterial, primary, secondary);
-    }
-
-    public void SkinButton2()
-    {
-        Color32 primary = new Color32(245, 220, 184, 255);
-        Color32 secondary = new Color32(96, 87, 75, 255);
-        ChangeMaterialColor(SkinMaterial, primary, secondary);
-    }
-
-    public void SkinButton3()
-    {
-        Color32 primary = new Color32(173, 111, 78, 255);
-        Color32 secondary = new Color32(79, 51, 36, 255);
-        ChangeMaterialColor(SkinMaterial, primary, secondary);
-    }
-
-    public void SkinButton4()
-    {
-        Color32 primary = new Color32(202, 160, 85, 255);
-        Color32 secondary = new Color32(96, 80, 50, 255);
-        ChangeMaterialColor(SkinMaterial, primary, secondary);
-    }
-
-    public void SkinButton5()
-    {
-        Color32 primary = new Color32(108, 91, 83, 255);
-        Color32 secondary = new Color32(53, 44, 40, 255);
-        ChangeMaterialColor(SkinMaterial, primary, secondary);
-    }
-
-    public void SkinButton6()
-    {
-        Color32 primary = new Color32(152, 66, 51, 255);
-        Color32 secondary = new Color32(82, 50, 44, 255);
-        ChangeMaterialColor(SkinMaterial, primary, secondary);
-    }
-
-    public void SkinButton7()
-    {
-        Color32 primary = new Color32(255, 187, 174, 255);
-        Color32 secondary = new Color32(99, 78, 74, 255);
-        ChangeMaterialColor(SkinMaterial, primary, secondary);
-    }
-
-    void ChangeMaterialColor(Material material, Color primary, Color secondary)
-    {
-        material.color = primary;
-        material.SetColor("_ColorDim", secondary);
+        material.color = SkinColors[index].main;
+        material.SetColor("_ColorDim", SkinColors[index].shaded);
     }
 }
