6ecc65f033b7f22f7979999daf3f6ff61788944e
David <d.weckerly@gmail.com>
2023-05-24 10:15:28 -0500

    prefab updates

--- Assets/Scripts/Core/StateMachine/Enemy/EnemyFightingState.cs
+++ Assets/Scripts/Core/StateMachine/Enemy/EnemyFightingState.cs
@@ -23,11 +23,11 @@ public class EnemyFightingState : EnemyBaseState
     {
         if (stateMachine.Player != null && stateMachine.playerStateMachine.Attributes.alive)
         {
-            if (stateMachine.Attributes.GetHPFraction() < 0.5f)
-            {
-                stateMachine.SwitchState(new EnemyFleeingState(stateMachine));
-                return;
-            }
+            // if (stateMachine.Attributes.GetHPFraction() < 0.5f)
+            // {
+            //     stateMachine.SwitchState(new EnemyFleeingState(stateMachine));
+            //     return;
+            // }
             FaceTarget(stateMachine.Player);
             if (!IsInAttackRange(stateMachine.Player))
             {
---
+++ Assets/Scripts/SFXNPC.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXNPC : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void FootStep() {}
+}
