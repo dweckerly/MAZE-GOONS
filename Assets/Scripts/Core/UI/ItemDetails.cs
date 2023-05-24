1023b8e8de7f6497e365892d51b42a4d4a033229
David <d.weckerly@gmail.com>
2023-05-24 12:52:57 -0500

    updated item detail props

--- Assets/Scripts/Core/UI/ItemDetails.cs
+++ Assets/Scripts/Core/UI/ItemDetails.cs
@@ -12,16 +12,22 @@ public class ItemDetails : MonoBehaviour
 
     public void ShowDetails(Item item)
     {
+        props.text = "";
         itemName.text = item.name;
         description.text = item.description;
+        
         if (item is Equippable)
         {
             if(((Equippable)item).equipped) actionBtnText.text = "Unequip";
             else actionBtnText.text = "Equip";
+            if (item is Weapon) props.text = "ATK - " + ((Weapon)item).weaponDamage;
+            if (item is Armor) props.text = "DEF - " + ((Armor)item).DamageReduction;
+            if (item is Shield) props.text = "DEF - " + ((Shield)item).DamageReduction;
         }
         else
         {
             actionBtnText.text = "Use";
         }
+        props.text += "\r\nWeight - " + item.weight;
     }
 }
