fae2de29ff055f4cc8fede94e0aee094a1b9c149
David <d.weckerly@gmail.com>
2023-06-21 08:00:58 -0500

    prevent enemies from hitting each other

--- Assets/Scripts/Core/WeaponDamage.cs
+++ Assets/Scripts/Core/WeaponDamage.cs
@@ -24,6 +24,7 @@ public class WeaponDamage : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other == sourceCollider) return;
+        if (other.CompareTag(sourceCollider.gameObject.tag)) return;
         if (alreadyCollidedWith.Contains(other)) return;
         alreadyCollidedWith.Add(other);
         if (other.TryGetComponent<Attributes>(out Attributes attributes))
